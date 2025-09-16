09b57341c021325c8749316a27b95f3d0ecf5c08
John Spanomanolis <john@fotonconsulting.com>
2025-09-16 16:12:14 +0300

    Fix nginx environment variable substitution
    - Add BACKEND_URL to envsubst command in start-nginx.sh
    - Fixes 'unknown backend_url variable' error in nginx
    - Now properly substitutes both PORT and BACKEND_URL variables
    - Resolves nginx startup failure in Render deployment
    - Tested envsubst command works correctly

--- frontend/start-nginx.sh
+++ frontend/start-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Replace PORT placeholder in nginx config
-envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+# Replace PORT and BACKEND_URL placeholders in nginx config
+envsubst '${PORT},${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start nginx
 nginx -g "daemon off;"
